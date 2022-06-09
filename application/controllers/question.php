c49e12865fe479f2a96cfde759621ab6f11e9b8b
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-06-09 22:39:55 +0700

    commit lagi

--- application/controllers/question.php
+++ application/controllers/question.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Collection extends MY_Controller
+class Question extends MY_Controller
 {
     public function __construct()
     {
